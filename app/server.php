1773f71c737abeb3028cbb77a78df7dc4a877d2b
Davide <bibbodavide@gmail.com>
2021-03-03 20:30:06 +0100

    aggiunti commit

--- app/server.php
+++ app/server.php
@@ -1,11 +1,12 @@
 <?php
 // il file server.php è il file che deve interrogare tramite axios.
-// si farà il json encode del mio database.
 
 require_once __DIR__ . '/../database/database.php';
 
+// variabile che cattura il valore di "genre" passato in $_GET
 $genreQuery = $_GET['genre'];
 
+// funzione che filtra gli album per genere, generando di volta in volta un nuovo array
 function filterByGenre($sourceArray, $genre) {
 
   $filtered = [];
@@ -20,9 +21,11 @@ function filterByGenre($sourceArray, $genre) {
   return $filtered;
 }
 
+// se il valore in $_GET non è vuoto, chiamo la funziona che filtra per genere
 if(!empty($genreQuery)){
   $discs = filterByGenre($discs, $genreQuery);
 }
 
 header('Content-Type: application/json');
+// si farà il json encode del mio database.
 echo json_encode($discs);
--- js/script.js
+++ js/script.js
@@ -16,11 +16,13 @@ new Vue({
 
     const self=this;
 
+    // chiamata axios a server.php
     axios.get('app/server.php')
     .then(function(resp){
       self.discs = resp.data;
       console.log(self.discs);
 
+      // creo array di generi
       self.discs.forEach((element)=>{
         if(!self.genres.includes(element.genre)){
           self.genres.push(element.genre)
@@ -30,9 +32,10 @@ new Vue({
   },
 
   methods: {
-
     genreSelect: function(){
+
       const self = this;
+
       console.log(self.select);
       if (self.select === 'Tutti') {
         axios.get('app/server.php')
